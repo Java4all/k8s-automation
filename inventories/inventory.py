7110074408563ade197645b3d2350041d3149a69
elesvi <elesvi@gmail.com>
2026-03-09 19:39:52 +0000

    add ssh support

--- inventories/inventory.py
+++ inventories/inventory.py
@@ -43,14 +43,17 @@ def generate_inventory(config):
     if 'masters' in config['nodes']:
         for master in config['nodes']['masters']:
             host = master['name']
-            inventory['masters']['hosts'][host] = {
+            master_host_config = {
                 'ansible_host': master['ip'],
                 'ansible_user': config['ssh']['user']
             }
-            inventory['all']['hosts'][host] = {
-                'ansible_host': master['ip'],
-                'ansible_user': config['ssh']['user']
-            }
+            # Add SSH key if specified for this node
+            if 'ssh_key_file' in master:
+                master_host_config['ansible_ssh_private_key_file'] = master['ssh_key_file']
+            
+            inventory['masters']['hosts'][host] = master_host_config
+            inventory['all']['hosts'][host] = master_host_config
+            
             inventory['_meta']['hostvars'][host] = {
                 'ansible_host': master['ip'],
                 'ansible_user': config['ssh']['user'],
@@ -58,19 +61,25 @@ def generate_inventory(config):
                 'node_role': 'master',
                 'cidr_prefix': netmask_to_cidr(master.get('netmask', '255.255.240.0'))
             }
+            # Add SSH key to hostvars if specified
+            if 'ssh_key_file' in master:
+                inventory['_meta']['hostvars'][host]['ansible_ssh_private_key_file'] = master['ssh_key_file']
     
     # Add worker nodes
     if 'workers' in config['nodes']:
         for worker in config['nodes']['workers']:
             host = worker['name']
-            inventory['workers']['hosts'][host] = {
+            worker_host_config = {
                 'ansible_host': worker['ip'],
                 'ansible_user': config['ssh']['user']
             }
-            inventory['all']['hosts'][host] = {
-                'ansible_host': worker['ip'],
-                'ansible_user': config['ssh']['user']
-            }
+            # Add SSH key if specified for this node
+            if 'ssh_key_file' in worker:
+                worker_host_config['ansible_ssh_private_key_file'] = worker['ssh_key_file']
+            
+            inventory['workers']['hosts'][host] = worker_host_config
+            inventory['all']['hosts'][host] = worker_host_config
+            
             inventory['_meta']['hostvars'][host] = {
                 'ansible_host': worker['ip'],
                 'ansible_user': config['ssh']['user'],
@@ -78,6 +87,9 @@ def generate_inventory(config):
                 'node_role': 'worker',
                 'cidr_prefix': netmask_to_cidr(worker.get('netmask', '255.255.240.0'))
             }
+            # Add SSH key to hostvars if specified
+            if 'ssh_key_file' in worker:
+                inventory['_meta']['hostvars'][host]['ansible_ssh_private_key_file'] = worker['ssh_key_file']
     
     return inventory
 
